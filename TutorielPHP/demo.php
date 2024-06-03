f9e9f693b86bb93971b96c1580e7ee371bca49c3
Groinkb <benjamin@mossu.ch>
2024-06-03 16:12:36 +0200

    condtions in php

--- TutorielPHP/demo.php
+++ TutorielPHP/demo.php
@@ -1,18 +1,9 @@
 <?php
-$classe = [
-    [
-        'nom' => 'Doe',
-        'prenom' => 'Jean',
-        'notes' => [16,16,16]
-    ],
-    [
-        'nom' => 'Doe',
-        'prenom' => 'Jane',
-        'notes' => [12,15,17]
-     ]
-    ];
-
-echo $classe[1]['notes'][1];
-?> 
+$note = 12;
+if($note >= 10) {
+    echo 'Bravo vous avez plus de 10';
+} else {
+    echo 'Dommage vous avez moins';
+}
 
 
