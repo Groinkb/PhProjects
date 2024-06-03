ccd3729df656b7afd87a7ac0bf14e72a7eb5bea6
Groinkb <benjamin@mossu.ch>
2024-06-03 17:17:38 +0200

    add switch to the course

--- TutorielPHP/demo.php
+++ TutorielPHP/demo.php
@@ -1,9 +1,29 @@
 <?php
-$note = 12;
-if($note >= 10) {
-    echo 'Bravo vous avez plus de 10';
-} else {
-    echo 'Dommage vous avez moins';
+$action = (int)readline('entrez votre action : (1: attaquer, 2: défendre, 3:passer mon tour)');
+
+switch ($action) {
+    case 1:
+        echo 'J\'attaque ! ';
+        break;
+    case 2:
+        echo 'Je défends';
+        break;
+    case 3:
+        echo 'Je ne fais rien';
+        break;
+    default:
+        echo 'Commande inconnue';    
 }
 
 
+
+if ($action ===1){
+    echo 'J\'attaque !';
+} elseif ($action ===2) {
+    echo 'Je défends';
+}
+elseif ($action ===3){
+    echo 'Je ne fais rien';
+} else{
+    echo 'Commande inconnue';
+}
