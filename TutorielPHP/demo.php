5b9df3af1de79dbfdcab9ec72393234d9ac83acb
Groinkb <benjamin@mossu.ch>
2024-06-05 16:37:38 +0200

    essai de la première fonction fait par moi

--- TutorielPHP/demo.php
+++ TutorielPHP/demo.php
@@ -1,29 +1,7 @@
 <?php
-$action = (int)readline('entrez votre action : (1: attaquer, 2: défendre, 3:passer mon tour)');
-// le switch c'est pour éviter les répitions d'if 
-switch ($action) {
-    case 1:
-        echo 'J\'attaque ! ';
-        break;
-    case 2:
-        echo 'Je défends';
-        break;
-    case 3:
-        echo 'Je ne fais rien';
-        break;
-    default:
-        echo 'Commande inconnue';    
+function bonjour($nom = 'Jean Marc'){
+    return 'Bonjour' . $nom . "\n";
 }
 
-//First commentary
-
-if ($action ===1){
-    echo 'J\'attaque !';
-} elseif ($action ===2) {
-    echo 'Je défends';
-}
-elseif ($action ===3){
-    echo 'Je ne fais rien';
-} else{
-    echo 'Commande inconnue';
-}
+$salutation = bonjour();
+echo $salutation;
